7d48595c14ee97c6a7211a8871ed0a05a78bf42d
intern <intern@RYAZAN>
2019-07-15 16:16:10 +0300

    Trying to load training model with ML.TensorFlow

---
+++ Testing/ImagePrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class ImagePrediction : ImageData
+    {
+        public float[] Score;
+
+        public string PredictedLabelValue;
+    }
+}
